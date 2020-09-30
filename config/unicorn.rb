05c1ad3399da1c0dc8a004f50ff7dcc820f25ef5
Yuichi Nakamura <yuichi.nakamura@jmty.jp>
2020-09-30 09:10:20 +0900

    pid, sockファイルの配置場所をRailsアプリのディレクトリ傘下にする

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,8 +3,8 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true  # 更新時ダウンタイム無し
 
-listen "/tmp/unicorn.sock"
-pid "/tmp/unicorn.pid"
+listen "/home/ec2-user/itamae-sample/tmp/unicorn.sock"
+pid "/home/ec2-user/itamae-sample/unicorn.pid"
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
@@ -28,4 +28,3 @@ end
 # ログの出力
 stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
 stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-
